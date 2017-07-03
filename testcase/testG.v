18bf5e90834749b14669e3fbd4ab9e9befd7dac4
mickyshey <r04943179@athena.ee.ntu.edu.tw>
2017-07-03 14:54:29 +0800

    add Example

---
+++ testcase/testG.v
@@ -0,0 +1,12 @@
+module top ( y , a , b , c );
+input a , b , c ;
+output y;
+wire g1 , g2 , g3 , g4 ;
+
+and ( g1 , a , b );
+and ( g2 , b , c );
+or ( g3 , g1 , g2 );
+and( g4 , a , c );
+or ( y , g3 , g4 );
+
+endmodule
